2a997ed39586e112c78e6e74b42f3862f1c94320
Xiaodong Li <bryne_lxd@sina.com>
2023-02-28 23:52:24 +0800

    added demo for useEffect

--- react-hooks-study/src/components/counterHook.js
+++ react-hooks-study/src/components/counterHook.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 function Counter(props) {
 
@@ -12,9 +12,26 @@ function Counter(props) {
         }
     )
 
+    //第二个是依赖数组，如果依赖数据的数据没有变化就不会被触发执行，
+    //如果是个空数组，那么useEffect只会在首次加载时被执行一次。
+    useEffect(() => {
+            console.log(count, '更新后的count')
+        }, []
+    )
+
+    //在回调函数里面如果return的话，会返回一个清理函数
+    useEffect(() => {
+            console.log('开始监听')
+            return () => {
+                console.log('取消监听')
+            }
+        }
+    )
+
     const [tc, setTc] = useState({name: 'allen', age: 30})
 
     const handleClick1 = () => {
+        console.log(count, '更新前的count')
         setCount (() => { 
             if (count < 3) {
                 return count + 1
