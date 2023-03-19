d0cfb5d9ec619762650cb2a104369c6ae86eb017
xiaodong <bryne_lxd@sina.com>
2023-03-19 21:45:57 +0800

    optimize the logic of adding new card

--- oh-my-kanban/src/App.js
+++ oh-my-kanban/src/App.js
@@ -2,21 +2,9 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react';
-import KanbanBoard from './KanbanBoard';
-import KanbanColumn from './KanbanColumn';
-
-
-const COLUMN_BG_COLORS = {
-  loading: '#E3E3E3',
-  todo: '#C9AF97',
-  ongoing: '#FFE799',
-  done: '#C0E8BA'
-};
+import KanbanBoard, { COLUMN_KEY_DONE, COLUMN_KEY_ONGOING, COLUMN_KEY_TODO } from './KanbanBoard';
 
 const DATA_STORE_KEY = 'kanban-data-store';
-const COLUMN_KEY_TODO = 'todo';
-const COLUMN_KEY_ONGOING = 'ongoing';
-const COLUMN_KEY_DONE = 'done';
 
 function App() {  
 
@@ -67,6 +55,12 @@ function App() {
     window.localStorage.setItem(DATA_STORE_KEY, data);
   };
 
+  const updaters =  {
+    [COLUMN_KEY_TODO]: setTodoList,
+    [COLUMN_KEY_ONGOING]: setOngoingList,
+    [COLUMN_KEY_DONE]: setDoneList
+  };
+
   //添加新任务提交时的处理，修改todoList数组，关闭Add组件的显示
   const handleAdd = (newCard) => {
     setTodoList( currentTodoList => [ 
@@ -75,29 +69,10 @@ function App() {
     ]);
   };
 
-  //记录拖拽过程的状态
-  const [draggedItem, setDraggedItem] = useState(null);
-  const [dragSource, setDragSource] = useState(null);
-  const [dragTarget, setDragTarget] = useState(null);
-
-  //处理拖拽的drop逻辑
-  const handleDrop = (evt) => {
-    if(!draggedItem || !dragSource || !dragTarget || dragSource === dragTarget){
-      return;
-    }
-    const updaters = {
-      [COLUMN_KEY_TODO]: setTodoList,
-      [COLUMN_KEY_ONGOING]: setOngoingList,
-      [COLUMN_KEY_DONE]: setDoneList
-    }
-    if (dragSource) {
-      updaters[dragSource]((currentStat) => 
-        currentStat.filter((item) => !Object.is(item, draggedItem))
-      )
-    }
-    if(dragTarget){
-      updaters[dragTarget]((currentStat) => [draggedItem, ...currentStat]);
-    }
+  const handleRemove = (column, cardToRemove) => {
+    updaters[column]((currentStat) =>
+      currentStat.filter((item) => !Object.is(item, cardToRemove))
+    );
   }
 
   return (    
@@ -106,42 +81,14 @@ function App() {
         <h1>我的看板<button onClick={handleSaveAll}>保存所有卡片</button></h1>        
         <img src={logo} className="App-logo" alt="logo" />
       </header>      
-      <KanbanBoard>
-        {isLoading ? (
-          <KanbanColumn title="读取中..." bgColor={COLUMN_BG_COLORS.loading} />
-        ) : (<>
-          <KanbanColumn 
-            bgColor={COLUMN_BG_COLORS.todo} 
-            canAddNew
-            title="待处理"
-            setDraggedItem={setDraggedItem}
-            setIsDragSource = {(isSrc) => setDragSource(isSrc ? COLUMN_KEY_TODO : null)}
-            setIsDragTarget = {(isTgt) => setDragTarget(isTgt ? COLUMN_KEY_TODO : null)}
-            onAdd={handleAdd}
-            onDrop={handleDrop}
-            cardList={todoList}
-          />
-          <KanbanColumn 
-            bgColor={COLUMN_BG_COLORS.ongoing} 
-            title="进行中"
-            setDraggedItem={setDraggedItem}
-            setIsDragSource = {(isSrc) => setDragSource(isSrc ? COLUMN_KEY_ONGOING : null)}
-            setIsDragTarget = {(isTgt) => setDragTarget(isTgt ? COLUMN_KEY_ONGOING : null)}
-            onDrop={handleDrop}
-            cardList={ongoingList}
-
-          />
-          <KanbanColumn 
-            bgColor={COLUMN_BG_COLORS.done} 
-            title="已完成"
-            setDraggedItem={setDraggedItem}
-            setIsDragSource = {(isSrc) => setDragSource(isSrc ? COLUMN_KEY_DONE : null)}
-            setIsDragTarget = {(isTgt) => setDragTarget(isTgt ? COLUMN_KEY_DONE : null)}
-            onDrop={handleDrop}
-            cardList={doneList}
-          />  
-        </>)}
-      </KanbanBoard>  
+      <KanbanBoard
+        isLoading={isLoading}
+        todoList={todoList}
+        ongoingList={ongoingList}
+        doneList={doneList}
+        onAdd={handleAdd}
+        onRemove={handleRemove}
+      />  
     </div>      
   );    
 }
